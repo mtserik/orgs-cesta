32cf0b393ba97236922d9bfd65fe639787be2ea3
mtserik <erik.stos.mts@gmail.com>
2023-01-10 17:13:48 -0300

    Criando a pasta mocks, mudando os textos para um arquivo e refatorando o código para utilizar os testos do objeto criado

---
+++ src/mocks/cesta.js
@@ -0,0 +1,17 @@
+
+import logo from '../../assets/logo.png';
+
+const cesta = {
+    topo: {
+        titulo: "Detalhes da cesta",
+    },
+    detalhes: {
+        nome: "Cesta de Verdura",
+        logoFazenda: logo,
+        nomeFazenda: "Jenny Jack Farm",
+        descricao: "Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha",
+        preco: "R$ 30,00",
+    }
+}
+
+export default cesta;
