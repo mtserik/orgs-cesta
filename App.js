35022219beee6f82f6800a0d7e28b354b25dabbf
mtserik <erik.stos.mts@gmail.com>
2023-01-10 16:54:29 -0300

    Adicionando componentes para a tela e os chamando no index

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StatusBar, SafeAreaView, View } from 'react-native';
 import { useFonts, Montserrat_400Regular, Montserrat_700Bold } from '@expo-google-fonts/montserrat';
 
-import Cesta from './src/telas/Cesta';
+import Cesta from './src/telas/Cesta/index';
 
 export default function App() {
   const [fonteCarregada] = useFonts({
